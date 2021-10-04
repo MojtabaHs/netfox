5bc00660e31bb4dc00cf907a31246ca6e48fc753
Seyed Mojtaba Hosseini Zeidabadi <chenzook@gmail.com>
2021-10-04 13:11:23 +0330

    add: spm support

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "NetFox",
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "NetFox",
+            targets: ["NetFox"]),
+    ],
+    dependencies: [],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "NetFox",
+            path: "netfox",
+            exclude: ["Core/NFXLoader.h", "Core/NFXLoader.m", "OSX"]
+        ),
+    ]
+)
